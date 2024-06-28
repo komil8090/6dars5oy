8baa8ac27f648a17d49171f8a0648e2d685b1f14
Mirkomil Mirvohidov <mirkomilmirvohidov4@gmail.com>
2024-06-28 14:38:42 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let color = prompt("textcolor kriting")
+let bgcolor = prompt("bgcolor kriting")
+
+let body = document.querySelector("body")
+let text = document.querySelector("p")
+
+body.style =`color: ${color}`
+text.style =`Background-color: ${bgcolor}`
